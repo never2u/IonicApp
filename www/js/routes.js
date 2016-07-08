7c0d408578677fd6e496834dccf3e0c52d692811
Jerry Lee <neverland77@gmail.com>
2016-07-08 15:33:32 +0900

    upload done

---
+++ www/js/routes.js
@@ -0,0 +1,85 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.page2', {
+    url: '/home',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/page2.html',
+        controller: 'page2Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController.page3', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/page3.html',
+        controller: 'page3Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController.page4', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/page4.html',
+        controller: 'page4Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.page5', {
+    url: '/page5',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/page5.html',
+        controller: 'page5Ctrl'
+      }
+    }
+  })
+
+  .state('tabsController.page6', {
+    url: '/page6',
+    views: {
+      'tab5': {
+        templateUrl: 'templates/page6.html',
+        controller: 'page6Ctrl'
+      }
+    }
+  })
+
+  .state('page7', {
+    url: '/page7',
+    templateUrl: 'templates/page7.html',
+    controller: 'page7Ctrl'
+  })
+
+  .state('page8', {
+    url: '/page8',
+    templateUrl: 'templates/page8.html',
+    controller: 'page8Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page1/home')
+
+  
+
+});
